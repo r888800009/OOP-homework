ebb0d8fd1f159d651ed4e5e9bb2165e0ea8c062c
r888800009 <r888800009@gmail.com>
2019-04-10 06:49:07 +0800

    Add src

---
+++ homework1/PlayDice.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class PlayDice {
+    public static boolean isThreeKind(Vector<Integer> dice) {
+        return false;
+    } // isSthreeKind
+
+    public static boolean isStraight(Vector<Integer> dice) {
+        return false;
+    } // isStraight
+
+    public static boolean isPair(Vector<Integer> dice) {
+        return false;
+    } // isPair
+
+    public static int sum(Vector<Integer> dice) {
+        return 0;
+    } // sum
+
+    public static void main(String[] args) {
+        Vector<Integer> dice = new Vector(); // save random number
+        int n = 0;
+
+        for (int i = 0; i < 3; i++) // get random number
+        {
+            n = (int) (Math.random() * 6 + 1);
+            dice.add(n);
+        } // for
+
+        if (isThreeKind(dice))
+            System.out.println("three of a kind");
+
+        else if (isStraight(dice))
+            System.out.println("straight");
+
+        else if (isPair(dice))
+            System.out.println("pair");
+
+        else
+            System.out.println("sum = " + sum(dice));
+
+    } // main
+
+} // PlayDice class
---
+++ homework1/TestPlayDice.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class TestPlayDice {
+    public static boolean testThreeKind() {
+        return false;
+    } // isSthreeKind
+
+    public static boolean testStraight() {
+        return false;
+    } // isStraight
+
+    public static boolean testPair() {
+        return false;
+    } // isPair
+
+    public static boolean testSum() {
+        return false;
+    } // sum
+
+    public static void main(String[] args) {
+        testThreeKind();
+        testStraight();
+        testPair();
+        testSum();
+
+    } // main
+
+} // TestPlayDice class
